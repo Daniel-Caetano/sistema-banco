f9f9adf8a56a66292aa8e2891a57065c668925fe
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-22 17:36:11 -0300

    Atualizado conta.hpp

--- Conta.hpp
+++ Conta.hpp
@@ -9,10 +9,16 @@ private:
   float saldo = 0;
 
 public:
+  Conta(std::string numeroConta, std::string cpfTitular,
+        std::string nomeTitular);
   void sacar(float valorSacar);
   void depositar(float valorDepositar);
-  float getSaldo();
+  float getSaldo() const;
+  void setSaldo();
   void setNome(std::string nome);
+  std::string getNome() const;
   void setCPF(std::string cpf);
+  std::string getCPF() const;
   void setNumero(std::string numero);
+  std::string getNumero() const;
 };

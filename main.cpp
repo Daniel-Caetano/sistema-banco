a517cb20468e99a5af86d1a35a173c33ade5bd36
unknown <danielcaetano.sf@gmail.com>
2021-11-24 15:03:09 -0300

    TESTE

--- main.cpp
+++ main.cpp
@@ -10,22 +10,21 @@ void exibeSalario(const Conta saldo) {
 
 int main() {
 
-  Conta umaConta("12345678", "986545497", "Daniel");
   Conta outraConta("12345678", "986545497", "Daniel");
   Conta maisUmaConta("12345678", "986545497", "Daniel");
   float saldo;
   // umaConta.setCPF("Daniel");
 
-  exibeSalario(umaConta);
+  // exibeSalario(umaConta);
   // saldo = umaConta.getSaldo();
 
   // cout << "\n Sistema Bancário " << endl;
   // cout << "SALDO: " << saldo << endl;
-  cout << "Nome: " << umaConta.getNome() << endl;
-  cout << "CPF: " << umaConta.getCPF() << endl;
-  cout << "Numero: " << umaConta.getNumero() << endl;
-  umaConta.depositar(350);
-  cout << "Saldo: " << umaConta.getSaldo() << endl;
+  // cout << "Nome: " << umaConta.getNome() << endl;
+  // cout << "CPF: " << umaConta.getCPF() << endl;
+  // cout << "Numero: " << umaConta.getNumero() << endl;
+  // umaConta.depositar(350);
+  // cout << "Saldo: " << umaConta.getSaldo() << endl;
   cout << "Qtd contas: " << Conta::getQtdContas() << endl;
   // umaConta.sacar(150);
   // umaConta.depositar(350);
